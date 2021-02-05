4df75c074f6bfb71795b01515022a99d8c737595
Eva Kuang <ekuang14@gmail.com>
2021-02-05 02:01:48 -0800

    finished

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,13 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	var name1 = request.query.name;
+	var description1 = request.query.description;
+	var newFriend = {
+		"name" : name1,
+		"description" : description1,
+		"imageURL" : "https://lorempixel.com/400/400.people"
+	}
+	data.friends.push(newFriend);
+	response.render('index', data);
  }
